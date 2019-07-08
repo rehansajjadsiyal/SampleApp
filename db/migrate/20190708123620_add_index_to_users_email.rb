0270ca4c76bdfa95f146374d35cc982b4d9a5726
rehansajjadsiyal <princerehan6632@gmail.com>
2019-07-08 08:50:54 -0400

    Modelling user is completed now

---
+++ db/migrate/20190708123620_add_index_to_users_email.rb
@@ -0,0 +1,5 @@
+class AddIndexToUsersEmail < ActiveRecord::Migration[6.0]
+  def change
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_08_071038) do
+ActiveRecord::Schema.define(version: 2019_07_08_123620) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
