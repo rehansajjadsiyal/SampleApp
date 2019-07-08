54e7ae14164aabc2c83439b90732919b11152e76
rehansajjadsiyal <princerehan6632@gmail.com>
2019-07-08 05:28:18 -0400

    sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   get  'static_pages/help'
   root 'static_pages#home'
   get 'static_pages/contact'
+  resources :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
