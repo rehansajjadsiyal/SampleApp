de496df26f560d96f73e45978e1ccfd1c8b774d4
rehansajjadsiyal <princerehan6632@gmail.com>
2019-07-08 07:56:50 -0400

    changes due to error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,3 @@
 class User < ApplicationRecord
-  before_save { email.downcase! }
-  validates :name,  presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },format:{with: VALID_EMAIL_REGEX},uniqueness: {case_sensitive: false}
-  has_secure_password
-  validates :password_digest, presence: true, length: {maximum: 6 }
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,11 +1,6 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
- def setup
-   @user=User.new(name:"Waheed Siyal",email:"waheedsiyal@gmail.com")
- end
 
-  # test "the truth" do
-  #   assert true
-  # end
+
 end
